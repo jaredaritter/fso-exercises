18c8c089b1c3f87b441089c6d90bb4a12cbd5b94
jared <38332939+jaredaritter@users.noreply.github.com>
2021-05-10 10:15:29 -0400

    Begin anecdotes exercise

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+// --------------------
+// APP COMPONENT
+// --------------------
+function App() {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ];
+
+  const zeroFilledArr = Array.apply(null, new Array(anecdotes.length)).map(
+    Number.prototype.valueOf,
+    0
+  );
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(zeroFilledArr);
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const handleNextClick = () => {
+    const num = getRandomInt(anecdotes.length);
+    console.log(num);
+    setSelected(num);
+  };
+
+  const handleVoteClick = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+    console.log(copy);
+  };
+
+  return (
+    <>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={handleVoteClick} text="vote" />
+      <Button handleClick={handleNextClick} text="next anecdote" />
+    </>
+  );
+}
+
+// --------------------------
+// OTHER COMPONENTS
+// --------------------------
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+export default App;
