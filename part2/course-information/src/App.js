8bc90e718559a5146b53ab5dd7099c169178cecc
jared <38332939+jaredaritter@users.noreply.github.com>
2021-05-11 12:10:54 -0400

    reinstall of course-information to correct errors from deleted files

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -30,24 +30,8 @@ const Content = ({ course }) => {
   );
 };
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
+const Course = (props) => {
+  const { course } = props;
 
   return (
     <div>
@@ -56,6 +40,32 @@ const App = () => {
       <Total course={course} />
     </div>
   );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
 };
 
 export default App;
