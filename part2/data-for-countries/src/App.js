85ac95919f33c2d6c34a2e1af073b0aab8f201b3
jared <38332939+jaredaritter@users.noreply.github.com>
2021-05-21 17:18:40 -0400

    Progress on data for countries exercise.

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const Filter = ({ filter, handleFilterChange }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries <input value={filter} onChange={handleFilterChange} />
     </div>
   );
-}
+};
+
+const Result = ({ countries }) => {
+  if (countries.length > 10) {
+    return <p>Too many matches, increase filter specificity</p>;
+  } else if (countries.length < 1) {
+    return <p>No countries match this filter criteria</p>;
+  } else if (countries.length === 1) {
+    return <h2>{countries[0].name}</h2>;
+  } else {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.numericCode}>{country.name}</li>
+        ))}
+      </ul>
+    );
+  }
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (e) => {
+    // console.log(e.target.value);
+    setFilter(e.target.value);
+  };
+
+  const effectHook = () => {
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      setCountries(response.data);
+    });
+  };
+
+  useEffect(effectHook, []);
+
+  const filteredCountries = countries.filter((country) => {
+    return country.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
+  return (
+    <>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Result countries={filteredCountries} />
+    </>
+  );
+};
 
 export default App;
