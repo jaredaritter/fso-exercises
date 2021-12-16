7ec1cfdea9a20cd448cd2dc1e079f0461e95c4c3
jared <38332939+jaredaritter@users.noreply.github.com>
2021-12-16 11:40:47 -0500

    Add middleware logger for improved client messaging of issue.

--- part4/blog-list-app/app.js
+++ part4/blog-list-app/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 logger.info('connecting to DB url');
 mongoose
@@ -19,9 +20,14 @@ mongoose
   });
 
 app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
